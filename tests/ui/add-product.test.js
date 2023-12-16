58623570f8a8a20291055e93af1dbd84b044592e
petarpachalov <pachalovpetar@gmail.com>
2023-12-16 16:33:59 +0200

    Changed with the new url

--- tests/ui/add-product.test.js
+++ tests/ui/add-product.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add products page', async ({ page }) => {
-    await page.goto('http://localhost:8080/Shopping-List');
+    await page.goto('https://pachalovpetar-exam.onrender.com/Shopping-List');
     const form = await page.$('ul');
     expect(form).toBeTruthy();
   });
